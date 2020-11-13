39f4868a627e145391d14941bb0bce50f964c882
Eliza <elisa.gau5@gmail.com>
2020-11-13 12:30:06 +0100

    Highlight Layer fix
    C'est tout bon wola. Faut ajouter les layers sur les bails qui brillent

--- Walking Sim URP/Assets/Full Body FPS Controller/Full Body FPS Controller/Scripts/Player_Interact.cs
+++ Walking Sim URP/Assets/Full Body FPS Controller/Full Body FPS Controller/Scripts/Player_Interact.cs
@@ -8,8 +8,7 @@ public class Player_Interact : MonoBehaviour
 
     public float range = 10f;
     public Camera fpsCam;
-
-    GameObject Cible;
+    public LayerMask shininglayer;
 
     Target target;
 
@@ -17,28 +16,26 @@ public class Player_Interact : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-    //    layerInteraction = LayerMask.NameToLayer("Interactable");
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
         RaycastHit hit;
-        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range, shininglayer))
         {
-            Cible = hit.transform.gameObject;
-            if (Cible.tag=="Collectibles" )
-            {
-                target = hit.transform.GetComponent<Target>();
-                target.Shine();
-            }
-            else if (target!=null && Cible.tag!="Collectibles" && target.shine==true)
-            {
-                target.StopShine();
-         
-            }
+            target = hit.transform.GetComponent<Target>();
+            target.Shine();
             
         }
+        else if (target!=null && target.shine==true)
+        {
+            target.StopShine();
+         
+        }
+            
+        
     }
 }
